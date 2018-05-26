ace5fbca020e38fabc9211ab8a2b80c4b7536040
David Coleman <david_coleman_007@hotmail.com>
2018-05-25 22:54:49 -0400

    initial code created a simple $(selector) fn to access dom elems created a simple ajax.get promise based util added the api key for my etsy app added basic api param parsing

---
+++ main.js
@@ -0,0 +1,74 @@
+document.onreadystatechange = () => {
+  const {readyState} = document;
+  console.log('document ready', readyState);
+  if(readyState === 'complete') {
+
+  }
+}
+
+const $ = selector => {
+  return document.querySelector(selector);
+}
+
+// please use Chrome and enable this plugin for local dev
+// https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en-US
+
+// I am serving this demo with:
+// py -m SimpleHTTPServer 8000
+
+const API_KEY = 'zm6wrrhrcnb9lkkkfkv9xb61';
+const API_BASE = 'https://openapi.etsy.com/v2';
+const LISTINGS_API = '/listings/active';
+const API_PARAM_KEY = 'api_key';
+
+const XHR = {
+  readyState: {
+    DONE : 4
+  },
+  status: {
+    OK : 200
+  }
+};
+
+const ajax = {
+  get : (api, data = {}) => {
+    return new Promise(
+      (resolve, reject) => {
+        const req = new XMLHttpRequest();
+        const parameters = Object.assign({
+          [API_PARAM_KEY]:API_KEY
+        },data);
+        console.log(data);
+        const pStr = '?'+
+          Object.entries(parameters).map(
+            ([key, value]) => `${key}=${encodeURI(value)}`
+          ).join('&');
+        console.log(pStr);
+        req.open('GET', `${API_BASE}/${api}${pStr}`);
+        req.send(null);
+        req.onreadystatechange = () => {
+          switch(req.readyState) {
+            case XHR.readyState.DONE:
+              if(req.status == XHR.status.OK) {
+                resolve(JSON.parse(req.responseText));
+              } else {
+                reject({
+                  error: req.status,
+                  message: req.responseText
+                });
+              }
+          }
+        }
+      }
+    )
+  }
+}
+
+onSearchClick = (event) => {
+  console.log($('#search-input'));
+  ajax.get(LISTINGS_API,{keywords:'sheep'}).then(
+    data => {
+      console.log(data);
+    }
+  )
+}
