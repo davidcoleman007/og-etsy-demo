f50570b70d4fe063db92684fa56d08af37b227cb
David Kenneth Coleman <david_coleman_007@hotmail.com>
2018-05-26 13:09:28 -0400

    very happy documentation added

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # og-etsy-demo
 og etsy exercise
+
+## How to run it
+use any static http server
+
+I used:
+
+```
+cd {project dir}
+py -m SimpleHTTPServer 8000
+```
+
+## What was done
+
+`main.js`
+
+  * on document ready we check the url to see if any search term is in the url
+  * `doSearch()`
+    * a function which sets the value of `#search-input` to whatever `?search` url parameter holds and invokes `runSearch()`
+  * `runSearch()`
+    * this function reads the `#search-input` element value and clears the children of the `.results-list` `<ul>`
+    * it then executes `ajax.get()` with the search term as specified in `#search-input`
+    * when the promise resolves to data, we itterate over the `results` key and build result items which are added as children of the `.results-list` `<ul>`
+  * `window.onpopstate` handles browser navigation
+  * `searchParams` handles getting url parameters from `location.href`
+  * `$` is a simple selector function that keeps syntax familiar and easy to use
+  * the `ajax` object contains a `get` method
+    * this returns a `Promise` which uses raw XHR to fetch the contents at the api endpoint specified
+    * this method assumes that all endpoints have a common `API_BASE`
+    * this method also supplies the `API_KEY` to every request
+    * this method also requests the association of `MainImage` for the purpose of displaying tile images
+  * the `createTemplate(id)` function
+    * this function looks for the template identified by `#{id}` and creates and returns a new instance of it
+  * `createItem(data)`
+    * this function takes a `result` from the  `results[]` array which is returned by
+    the `LISTINGS_API` and applies the necessary data to an instance of `#etsy-item-template` as created by `createTemplate`
+  * `onSearchClick()` does exactly what it sounds like it does - it handles the click on the search button
+    * it also saves the new search in the `window.history` and invokes `runSearch()`
+
+`index.html`
+  * a simple HTML5 doc
+  * it contains a main header with the following elements:
+    * `.search`
+      * this is the page header
+    * `.search-input`
+      * where we type the search terms
+    * generic button which executes searches
+  * it contains a `.main-results` element
+    * this contains a `<header>` which generically says "Search Results"
+    * this contains a `.results` `<section>`
+      * the results section contains an HTML `<template>` which is used to build each results tile when search results are retrieved from the api
+      * the results section contains an empty `<ul>` identified by `.results-list`
+        * this ul is populated with the instances of `template#etsy-item-template` created by the `createItem()` function
+
+## Summary
+I choose to implement everything from scratch since this was the requirement.
+
+Additionally, I choose to implement the `$` function to make the code easier to read and more familiar.
+
+I choose to use HTML Templates to avoid long and dense `document.createElement` chains. I felt it was much more elegant and compact.
+
+I choose to use semantic HTML because it is proper to do so.  :)
+
+I was considering implementing some things like `<ENTER>` to search but I ran out of time and the exercise was time-boxed to 2-3 hours and I needed to leave myself at least 30 mins to write up this file and document my code.
+
+I implemented bookmarkable search results as well as the ability to navigate fwd/back w/o redirect.
+
+Styles are minimal and use etsy orange.
--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-document.onreadystatechange = () => {
-  const {readyState} = document;
-  if(readyState === 'complete') {
-    doSearch();
-  }
-}
-
-const doSearch = () => {
-  const search = searchParams().get('search');
-  if(search) {
-    $('#search-input').value = search;
-    runSearch();
-  }
-}
-
-window.onpopstate = (event) => {
-  doSearch();
-}
-
-const searchParams = () => {
-  const urlStr = window.location.href;
-  const url = new URL(urlStr);
-  return url.searchParams;
-}
-
-const $ = selector => {
-  return document.querySelector(selector);
-}
-
-// please use Chrome and enable this plugin for local dev
-// https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en-US
-
-// I am serving this demo with:
-// py -m SimpleHTTPServer 8000
-
 const API_KEY = 'zm6wrrhrcnb9lkkkfkv9xb61';
 const API_BASE = 'https://openapi.etsy.com/v2';
 const LISTINGS_API = '/listings/active';
@@ -47,7 +12,100 @@ const XHR = {
   }
 };
 
+/**
+ * on document ready we check the url to see if any
+ * search term is in the url
+ */
+document.onreadystatechange = () => {
+  const {readyState} = document;
+  if(readyState === 'complete') {
+    doSearch();
+  }
+}
+
+/**
+ * a function which sets the value of `#search-input` to
+ * whatever `?search` url parameter holds and invokes
+ * `runSearch()`
+ */
+const doSearch = () => {
+  const search = searchParams().get('search');
+  if(search) {
+    $('#search-input').value = search;
+    runSearch();
+  }
+}
+
+/**
+ * the main search function
+ *   * this function reads the `#search-input` element value and
+ *   clears the children of the `.results-list` `<ul>`
+ *   * it then executes `ajax.get()` with the search term as specified
+ *   in `#search-input`
+ *   * when the promise resolves to data, we itterate over the `results`
+ *   key and build result items which are added as children of the
+ *   `.results-list` `<ul>`
+ */
+const runSearch = () => {
+  const {value: searchText} = $('#search-input');
+  const resultsList = $('.results-list');
+  while(resultsList.firstChild) {
+    resultsList.firstChild.remove();
+  }
+  ajax.get(LISTINGS_API,{keywords:searchText}).then(
+    data => {
+      data.results.forEach(
+        result => {
+          const item = createItem(result);
+          resultsList.appendChild(item);
+        }
+      )
+    }
+  ).catch(
+    err => {
+      console.error('ERROR!', err);
+    }
+  );
+}
+
+/**
+ * handles browser navigation
+ */
+window.onpopstate = () => {
+  doSearch();
+}
+
+/**
+ * handles getting url parameters from `location.href`
+ */
+const searchParams = () => {
+  const urlStr = window.location.href;
+  const url = new URL(urlStr);
+  return url.searchParams;
+}
+
+/**
+ * a simple selector function that keeps syntax familiar and easy to use
+ * @param {String} selector css selector to query in document
+ */
+const $ = selector => {
+  return document.querySelector(selector);
+}
+
+/**
+ * this object is a simple ajax util
+ */
 const ajax = {
+  /**
+   * Implements ajax for GET requests
+   * * this returns a `Promise` which uses raw XHR to fetch the contents at the api endpoint specified
+   * * this method assumes that all endpoints have a common `API_BASE`
+   * * this method also supplies the `API_KEY` to every request
+   * * this method also requests the association of `MainImage` for the purpose of displaying tile images
+   *
+   * @param {String} api the specific api which you want to call
+   * @param {Object} data the url parameters to call the api with
+   **/
   get : (api, data = {}) => {
     return new Promise(
       (resolve, reject) => {
@@ -85,11 +143,24 @@ const ajax = {
   }
 }
 
+/**
+ * this function looks for the template identified by `#{id}`
+ * and creates and returns a new instance of it
+ * @param {String} id
+ */
 const createTemplate = id => {
   const template = $(`#${id}`);
   return document.importNode(template.content, true);
 }
 
+/**
+ * this function takes a `result` from the  `results[]` array
+ * which is returned by the `LISTINGS_API` and applies the
+ * necessary data to an instance of `#etsy-item-template` as
+ * created by `createTemplate`
+ *
+ * @param {Object} data a `result` returned by the `LISTINGS_API`
+ */
 const createItem = data => {
   const item = createTemplate('etsy-item-template')
   const summary = item.querySelector('summary');
@@ -99,29 +170,14 @@ const createItem = data => {
   return item;
 }
 
-const runSearch = () => {
-  const {value: searchText} = $('#search-input');
-  const resultsList = $('.results-list');
-  while(resultsList.firstChild) {
-    resultsList.firstChild.remove();
-  }
-  ajax.get(LISTINGS_API,{keywords:searchText}).then(
-    data => {
-      data.results.forEach(
-        result => {
-          const item = createItem(result);
-          resultsList.appendChild(item);
-        }
-      )
-    }
-  ).catch(
-    err => {
-      console.error('ERROR!', err);
-    }
-  );
-}
-
-const onSearchClick = (event) => {
+/**
+ * does exactly what it sounds like it does - it handles the
+ * click on the search button
+ *
+ * it also saves the new search in the `window.history` and
+ * invokes `runSearch()`
+ */
+const onSearchClick = () => {
   window.history.pushState(
     searchParams(),
     `Search Results For ${searchText}`,
